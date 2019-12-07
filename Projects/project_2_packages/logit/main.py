cc6f2f209d81d075769db8cd61f5ef8f36fb110d
Jett Pettus <jettpettus@gmail.com>
2019-12-07 16:47:48 -0500

    added logit that kinda sorta works

--- Projects/project_2_packages/logit/main.py
+++ Projects/project_2_packages/logit/main.py
@@ -1,31 +1,44 @@
 import numpy as np
-import math
+from math import log, exp
 from scipy.optimize import minimize
 
 
 def ll_function (x, y, beta):
     # Reshape beta
-    beta = beta[:, None]
-    beta_prime = np.transpose(beta)
-    fun_1 = y @ beta_prime @ x
-    fun_2 = math.log(1 + math.exp(beta_prime @ x))
-    fun = np.subtract(fun_1, fun_2)
-    fun = fun.sum()
-    return fun
 
+    ll = 0
+    for i in range(1, x.shape[0]):
+        ll = ll + exp(np.dot(x[i, :], beta)) + y[i] * np.dot(x[i, :], beta)
+    return ll
 
 def fit_logit(x, y):
-    beta_init = np.random.rand(len(x), 1)
+    beta_init = np.random.rand(x.shape[1], 1)
     min_func = (lambda beta: ll_function(x, y, beta))
     res = minimize(min_func, x0 = beta_init)
     return res
 
+def logit_se(fit):
+    hessian_inv = fit.hess_inv
+    se = np.sqrt(np.diag(hessian_inv))
+    return se
 
 if __name__ == "__main__":
-    x = np.transpose(np.matrix([0, 1, 3, 0.5, 3, -1, 2]))
+    print("x")
+    x = np.transpose(np.matrix([[0, 1, 3, 0.5, 3, -1, 2],
+                               [1, 3, .2, 1, 4, 4, 1 ],
+                               [2, 4, 9, 1, 0.2, 1, .4]]))
+    print("dot")
+    print(np.dot(x[1, :],  [1, 2, 3]))
+    print(x)
+    print(x.shape)
+    print("y")
     y = np.transpose(np.matrix([9, 4, 23, 4, 4, -2, 1]))
-    
-    print(fit_logit(x, y))
-
-
-
+    print("llfunct")
+    ll_function(x, y, beta = [1, 2, 3])
+    print(y)
+    print(y.shape)
+    fit = fit_logit(x, y)
+    print(fit)
+    print("se")
+    print(logit_se(fit))
+    # print(fit_logit(x, y))
